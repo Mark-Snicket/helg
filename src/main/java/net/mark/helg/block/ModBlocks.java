7562661b2567135606de0998e957f7ab7e4cc969
Mark <3mark.7395g@gmail.com>
2025-12-28 22:48:11 +0100

    - added 2 horse armor types, helg & helgerite - renamed the armor material type from helg to helgerite and added helg as material - removed helgerite set effect (that wasn't even meant to be there) - added loot table modifier to add helg horse armor to chests that can contain other horse armor - renamed helg_block & helg_ingot to helgerite_block & helgerite_ingot - added helgerite_craftables for ingot & block - renamed all recipes files for block & ingot from helg to helgerite - renamed all recipes contents for block & ingot from helg to helgerite and replaced helg ingot and block item usage with the respecting tag - added issues link to fabric.mod.json

--- src/main/java/net/mark/helg/block/ModBlocks.java
+++ src/main/java/net/mark/helg/block/ModBlocks.java
@@ -27,8 +27,17 @@ public class ModBlocks {
                     .requiresTool()
                     .sounds(BlockSoundGroup.ROOTED_DIRT)));
 
-    public static final Block HELG_BLOCK = registerBlock("helg_block",
+    public static final Block HELGERITE_BLOCK = registerBlock("helgerite_block",
             properties -> new Block(properties
+                    .strength(5f)
+                    .resistance(5f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.GRAVEL)));
+
+
+    /* temp block to handle "changing the id" */
+    public static final Block HELG_BLOCK = registerBlock("helg_block",
+                    properties -> new Block(properties
                     .strength(5f)
                     .resistance(5f)
                     .requiresTool()
@@ -56,7 +65,7 @@ public class ModBlocks {
         });
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-            entries.add(ModBlocks.HELG_BLOCK);
+            entries.add(ModBlocks.HELGERITE_BLOCK);
         });
     }
 }
--- src/main/java/net/mark/helg/item/ModArmorMaterials.java
+++ src/main/java/net/mark/helg/item/ModArmorMaterials.java
@@ -12,14 +12,27 @@ import net.minecraft.util.Util;
 import java.util.EnumMap;
 
 public class ModArmorMaterials {
+
     public static EquipmentModel HELGERITE = EquipmentModel.builder().addHumanoidLayers(Identifier.of(Helg.MOD_ID, "helgerite")).build();
 
     public static final ArmorMaterial HELGERITE_ARMOR_MATERIAL = new ArmorMaterial(25, Util.make(new EnumMap<>(EquipmentType.class), map -> {
-        map.put(EquipmentType.BOOTS,2);
-        map.put(EquipmentType.LEGGINGS,5);
-        map.put(EquipmentType.CHESTPLATE,7);
-        map.put(EquipmentType.HELMET,3);
-        map.put(EquipmentType.BODY,10);
-    }), 15, SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA, 1, 0, ModTags.Items.HELGERITE_REPAIR,
-            Identifier.of(Helg.MOD_ID, "helgerite"));
+        map.put(EquipmentType.BOOTS, 2);
+        map.put(EquipmentType.LEGGINGS, 5);
+        map.put(EquipmentType.CHESTPLATE, 7);
+        map.put(EquipmentType.HELMET, 3);
+        map.put(EquipmentType.BODY, 13);
+    }), 15, SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA, 1.0f, 0.0f, ModTags.Items.HELGERITE_ARMOR_REPAIRABLES, Identifier.of(Helg.MOD_ID, "helgerite"));
+
+
+    public static EquipmentModel HELG = EquipmentModel.builder().addHumanoidLayers(Identifier.of(Helg.MOD_ID, "helg")).build();
+
+    public static final ArmorMaterial HELG_ARMOR_MATERIAL = new ArmorMaterial(25, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+        map.put(EquipmentType.BOOTS, 2);
+        map.put(EquipmentType.LEGGINGS, 5);
+        map.put(EquipmentType.CHESTPLATE, 7);
+        map.put(EquipmentType.HELMET, 3);
+        map.put(EquipmentType.BODY, 13);
+    }), 15, SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA, 1.0f, 0.0f, ModTags.Items.HELGERITE_ARMOR_REPAIRABLES, Identifier.of(Helg.MOD_ID, "helg"));
+
+
 }
