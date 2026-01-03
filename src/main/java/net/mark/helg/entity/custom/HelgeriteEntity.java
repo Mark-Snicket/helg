a794cfd85d4f2a3c8a218feb9b866cc25df62068
Mark <3mark.7395g@gmail.com>
2026-01-03 02:59:43 +0100

    - fixed ArmorTemptGoal.java, so that is now finally in use, meaning Helgerites are now finally tempted by wearing the full set of armor
    - reorganized HelgeriteEntity.initGoals(), lowering the priority of FlyGoal, so it no longer flies quickly when tempted
    
    - renamed createAttributes() to createHelgeriteAttributes() and updated default attributes

--- src/main/java/net/mark/helg/entity/custom/HelgeriteEntity.java
+++ src/main/java/net/mark/helg/entity/custom/HelgeriteEntity.java
@@ -1,6 +1,7 @@
 package net.mark.helg.entity.custom;
 
-import net.mark.helg.block.ModBlocks;
+import net.mark.helg.entity.custom.goal.ArmorTemptGoal;
+import net.mark.helg.item.ModArmorMaterials;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.control.FlightMoveControl;
@@ -10,12 +11,10 @@ import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
@@ -36,14 +35,13 @@ public class HelgeriteEntity extends AnimalEntity {
 
     @Override
     protected void initGoals() {
-        this.goalSelector.add(0, new FlyGoal(this, 3.0d));
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25));
-        //this.goalSelector.add(2, new ArmorTemptGoal(this, 1.33d, ModArmorMaterials.HELGERITE_ARMOR_MATERIAL));
-        this.goalSelector.add(2, new TemptGoal(this, 1.33d, Ingredient.ofItems(ModBlocks.HELGERITE_BLOCK, ModBlocks.HELG_BLOCK), false));
-        this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 6.0f));
-        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.0d));
-        //this.goalSelector.add(5, new LookAroundGoal(this));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25f));
+        this.goalSelector.add(2, new ArmorTemptGoal(this, 1.5f, ModArmorMaterials.HELGERITE_ARMOR_MATERIAL, false));
+        this.goalSelector.add(3, new FlyGoal(this, 3.0f));
+        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.0f));
+        this.goalSelector.add(5, new LookAroundGoal(this));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0f));
 
     }
 
@@ -60,24 +58,23 @@ public class HelgeriteEntity extends AnimalEntity {
         return birdNavigation;
     }
 
-    public static DefaultAttributeContainer.Builder createAttributes() {
-        return MobEntity.createMobAttributes()
+    public static DefaultAttributeContainer.Builder createHelgeriteAttributes() {
+        return AnimalEntity.createAnimalAttributes()
                 .add(EntityAttributes.MAX_HEALTH, 10)
                 .add(EntityAttributes.MOVEMENT_SPEED, 0.2)
                 .add(EntityAttributes.FLYING_SPEED, 0.33)
-                .add(EntityAttributes.SAFE_FALL_DISTANCE, 9999.0)
-                .add(EntityAttributes.TEMPT_RANGE, 16);
+                .add(EntityAttributes.FALL_DAMAGE_MULTIPLIER, 0.0f)
+                .add(EntityAttributes.GRAVITY, 0.0f);
     }
 
     private void setupAnimationStates() {
         if (this.idleAnimationCooldown <= 0) {
             this.idleAnimationCooldown = 240;
             this.idleAnimationState.start(this.age);
-        }else {
+        } else {
             --this.idleAnimationCooldown;
         }
     }
-
 
 
     @Override
