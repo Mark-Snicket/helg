a7f9ee8ee973cd883177459d48ca720706420c2a
Mark <3mark.7395g@gmail.com>
2026-02-22 03:34:41 +0100

    replaced deprecated api call with a built-in one

--- src/main/java/net/mark/helg/HelgClient.java
+++ src/main/java/net/mark/helg/HelgClient.java
@@ -2,16 +2,16 @@ package net.mark.helg;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.mark.helg.entity.ModEntities;
 import net.mark.helg.entity.client.helgerite.HelgeriteModel;
 import net.mark.helg.entity.client.helgerite.HelgeriteRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 
 public class HelgClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityModelLayerRegistry.registerModelLayer(HelgeriteModel.HELGERITE, HelgeriteModel::getTexturedModelData);
-        EntityRendererRegistry.register(ModEntities.HELGERITE, HelgeriteRenderer::new);
+        EntityRenderers.register(ModEntities.HELGERITE, HelgeriteRenderer::new);
 
     }
 }
