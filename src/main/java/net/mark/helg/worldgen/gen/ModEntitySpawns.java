4b03c584ea4de0814b822aba6a9f1f765c19b682
Mark <3mark.7395g@gmail.com>
2026-09-13 00:51:05 +0200

    - renamed folder "world" to "worldgen" - removed ModWorldGeneration class and added its function to Helg.java - removed unused logger from Helg.java

--- src/main/java/net/mark/helg/worldgen/gen/ModEntitySpawns.java
+++ src/main/java/net/mark/helg/worldgen/gen/ModEntitySpawns.java
@@ -1,11 +1,10 @@
-package net.mark.helg.world.gen;
+package net.mark.helg.worldgen.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.mark.helg.entity.ModEntities;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnLocationTypes;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.world.Heightmap;
