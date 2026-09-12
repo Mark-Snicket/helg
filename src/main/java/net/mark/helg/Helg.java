729d9b4d57bd081233179a8b412ef4f2dfdc69fd
Mark <3mark.7395g@gmail.com>
2026-09-13 00:49:27 +0200

    - renamed folder "world" to "worldgen" - removed ModWorldGeneration class and added its function to Helg.java - removed unused logger from Helg.java

--- src/main/java/net/mark/helg/Helg.java
+++ src/main/java/net/mark/helg/Helg.java
@@ -1,27 +1,26 @@
 package net.mark.helg;
 
 import net.fabricmc.api.ModInitializer;
-
-
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.mark.helg.block.ModBlocks;
 import net.mark.helg.entity.ModEntities;
 import net.mark.helg.entity.custom.HelgeriteEntity;
 import net.mark.helg.item.ModItems;
 import net.mark.helg.util.ModLootTableModifiers;
-import net.mark.helg.world.gen.ModWorldGeneration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.mark.helg.worldgen.gen.ModEntitySpawns;
+import net.mark.helg.worldgen.gen.ModOreGeneration;
 
 public class Helg implements ModInitializer {
 	public static final String MOD_ID = "helg";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-		ModWorldGeneration.generateModWorldGen();
+
+		ModOreGeneration.generateOres();
+		ModEntitySpawns.addSpawns();
+
 		ModEntities.registerModEntities();
 		ModLootTableModifiers.modifyLootTables();
 
--- src/main/java/net/mark/helg/worldgen/gen/ModOreGeneration.java
+++ src/main/java/net/mark/helg/worldgen/gen/ModOreGeneration.java
@@ -1,8 +1,8 @@
-package net.mark.helg.world.gen;
+package net.mark.helg.worldgen.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.mark.helg.world.ModPlacedFeatures;
+import net.mark.helg.worldgen.ModPlacedFeatures;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
